db9535e55192de72cac0b49d6f970f84f4269e63
Raul Oliva <oliva.raul12@gmail.com>
2023-05-28 10:23:56 -0600

    * importing SVG images * testing routing * importing fonts

---
+++ src/app/dashboard/layout.js
@@ -0,0 +1,12 @@
+import SettingsPage from './page'
+
+const SettingsLayout = () => (
+    <main>
+        <div>
+            <p>My Nav</p>
+        </div>
+        <SettingsPage />
+    </main>
+)
+
+export default SettingsLayout
---
+++ src/app/dashboard/page.js
@@ -0,0 +1,13 @@
+const Settings = () => {
+    return (
+        <>
+            <h2>My Settings</h2>
+            <div>
+                <p>These are my settings</p>
+            </div>
+        </>
+        
+    )
+}
+
+export default Settings
